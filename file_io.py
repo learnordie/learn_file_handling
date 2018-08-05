cf800f45c7ea8c93b84b5493931f4797a4b0fc9a
learnordie <learnordie@yandex.com>
2018-08-05 09:15:40 +0000

    Add some code about how to write and how to append files

--- file_io.py
+++ file_io.py
@@ -8,6 +8,7 @@ OUTPUT_FILE = "output.txt"
 # NOFILE = "dioooos.txt"
 
 
+# Read the complete file at once
 with open(INPUT_FILE) as f:
     content = f.read()
 
@@ -18,6 +19,7 @@ print(content)
 
 # print(content)
 
+# Read the file line by line
 with open(INPUT_FILE) as f:
     first_line = f.readline()
     second_line = f.readline()
@@ -27,11 +29,18 @@ print("First line: ", first_line)
 print("Second line: ", second_line)
 print("Third line: ", third_line)
 
+# Write into a file that could exist or not
 with open(OUTPUT_FILE, "w") as f:
     f.write(third_line)
     f.write(second_line)
     f.write(first_line)
 
+# Append into a file that could exist or not
+with open(OUTPUT_FILE, "a") as f:
+    f.write("Appending some_text...\n")
+    f.write(first_line)
+    f.write(second_line)
+    f.write(third_line)
 
 with open(OUTPUT_FILE) as f:
     content2 = f.read()
--- text.py
+++ text.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" How to format some text """
+""" How to format some text using regex """
